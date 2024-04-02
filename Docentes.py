905b7e7ed8d946882f99dd5b61c3ad1f72f151ee
TalismanTeixeira <163419886+TalismanTeixeira@users.noreply.github.com>
2024-04-01 23:06:43 -0300

    Update Docentes.py

--- Docentes.py
+++ Docentes.py
@@ -12,7 +12,7 @@ docentes = pd.read_excel('Docentes IFPA 2024.xlsx')
 docentes['Área de ingresso']=docentes['Área de ingresso'].str.capitalize()
 docentes_por_area=docentes.groupby(['Campus','Área de ingresso'])['Docente'].count().reset_index()
 fig=px.sunburst(docentes_por_area, path=['Campus', 'Área de ingresso'], values='Docente',
-    color='Campus', title='<b>Docentes do IFPA por campus/área</b>', template='plotly_dark')
+    color='Campus', template='plotly_dark')
 fig.update_traces(hovertemplate='%{label}<br>Docentes=%{value}<extra></extra>')
 fig.update_layout(width=1000, height=1000, font_color='white',uniformtext_minsize=12, uniformtext_mode='hide', title_x = 0.5, title_font_size=24 )
 fig.update_traces(textfont_color='white')
@@ -21,4 +21,5 @@ fig.add_layout_image(dict(source=Logo,
         x=0.2, y=1.08,
         sizex=0.2, sizey=0.2,
         xanchor="right", yanchor="top"))
+st.title('Docentes do IFPA por campus/área')
 st.plotly_chart(fig)
