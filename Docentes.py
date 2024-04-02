cdeb761615310442c5d1012f46944b66322031ea
TalismanTeixeira <163419886+TalismanTeixeira@users.noreply.github.com>
2024-04-01 23:11:26 -0300

    Update Docentes.py

--- Docentes.py
+++ Docentes.py
@@ -16,10 +16,6 @@ fig=px.sunburst(docentes_por_area, path=['Campus', 'Área de ingresso'], values=
 fig.update_traces(hovertemplate='%{label}<br>Docentes=%{value}<extra></extra>')
 fig.update_layout(width=1000, height=1000, font_color='white',uniformtext_minsize=12, uniformtext_mode='hide', title_x = 0.5, title_font_size=24 )
 fig.update_traces(textfont_color='white')
-fig.add_layout_image(dict(source=Logo,
-           xref="paper", yref="paper",
-        x=0.2, y=1.05,
-        sizex=0.2, sizey=0.2,
-        xanchor="right", yanchor="top"))
+st.image('Logo.png')
 st.title('Docentes do IFPA por campus/área')
 st.plotly_chart(fig)
